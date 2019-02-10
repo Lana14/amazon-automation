a07d02bab9e578fee40aec15aae92f4a93bb7aa1
Ruslana Zhulinska <ruslana.zhulinska@onedome.com>
2019-02-10 10:17:50 +0000

    Add parameterizing

--- src/test/java/ua/startit/BaseTest.java
+++ src/test/java/ua/startit/BaseTest.java
@@ -1,19 +1,31 @@
 package ua.startit;
 
-
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import org.testng.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import ua.startit.support.Properties;
+
 
 public class BaseTest {
 
-    protected static final String URL = "https://www.amazon.com";
+    private Properties properties = Properties.getInstance();
+
+    private  static final Logger LOG = LoggerFactory.getLogger("BaseTest");
 
     @BeforeSuite(alwaysRun = true)
     public void setEnv() {
-        Configuration.browser = "chrome";
+        Configuration.browser = properties.getBrowser();
         Configuration.timeout = 10000;
-        Configuration.baseUrl = URL;
+        Configuration.baseUrl = properties.getEnv();
+        Configuration.headless = Boolean.parseBoolean(properties.getHeadless());
+
+        LOG.info("Browser: " + properties.getBrowser());
+        LOG.info("Env: " + properties.getEnv());
+        LOG.info("Headless is " + Configuration.headless);
     }
 
     @BeforeMethod(alwaysRun = true)
